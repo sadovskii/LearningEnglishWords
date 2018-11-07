c4e575e92d2a6096cfe1ac3b41004bb556ea52d3
anton <anton.sadovskiy.97@mail.ru>
2018-11-07 11:20:56 +0300

    interim commit

--- ExerciseForLearningWords/wwwroot/js/site.js
+++ ExerciseForLearningWords/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your JavaScript code.
+﻿$(".test_1").click(function () {
+    $(".test_div_1").hide();
+    $(".test_div_2").show();
+});
+
+$(".test_2").click(function () {
+    $(".test_div_1").show();
+    $(".test_div_2").hide();
+});
