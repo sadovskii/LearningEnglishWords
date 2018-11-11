2c2ae5f078ba5f0f7df82b91d19f4c3b1cf5e2b1
anton <anton.sadovskiy.97@mail.ru>
2018-11-11 21:03:10 +0300

    version 1 pushed

---
+++ ExerciseForLearningWords/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿$(".test_1").click(function () {
+    $(".test_div_1").css('visibility', 'hidden');
+    $(".test_div_2").css('visibility', 'visible');
+});
+
+$(".test_2").click(function () {
+    $(".test_div_1").css('visibility', 'visible');
+    $(".test_div_2").css('visibility', 'hidden');
+});
+
+$(".test_3").click(function () {
+    $(".test_div_1").css('visibility', 'visible');
+    $(".test_div_2").css('visibility', 'visible');
+});
+
+
